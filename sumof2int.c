09db1c2e741edde847c2223384e4c2c6c667c204
tpsingh15 <36785798+tpsingh15@users.noreply.github.com>
2018-10-02 18:37:13 +0530

    Create sumof2int.c

---
+++ sumof2int.c
@@ -0,0 +1,11 @@
+//add two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the two numbers:\n");
+    scanf("%d%d",&a,&b);
+    printf("sum= %d",a+b);
+    return 0;
+    
+}
